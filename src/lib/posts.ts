32c0a4f89765895fc29c1c5b4abe68bc3289d267
Vojta Holik <vojta@egghead.io>
2024-03-20 11:20:26 +0100

    fix: base url

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -2,10 +2,10 @@ import { getBaseUrl } from '@/utils/get-base-url'
 
 export async function getPosts() {
   try {
-    const posts = await fetch(`${getBaseUrl()}/api/posts`).then((res) =>
-      res.json()
-    )
-
+    const posts = await fetch(`${getBaseUrl()}/api/posts`, {
+      method: 'GET',
+    }).then((res) => res.json())
+    console.log({ posts })
     return posts
   } catch (error) {
     console.error(error)
